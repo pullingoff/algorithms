f24b1ffd63fe1b3444a98c67332e597e7fb797a3
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2023-12-29 13:03:41 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 10.94 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.js
@@ -0,0 +1,13 @@
+function solution(r1, r2) {
+    var answer = 0;
+    
+    for (let i = 1; i <= r2; i++) {
+        let max = Math.floor(Math.sqrt(r2**2 - i**2));
+        let min = i >= r1 
+                    ? 0 
+                    : Math.ceil(Math.sqrt(r1**2 - i**2));
+        
+        answer = answer + max - min + 1;
+    }
+    return answer * 4;
+}
