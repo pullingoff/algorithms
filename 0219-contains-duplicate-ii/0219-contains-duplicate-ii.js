0dae5cff18df66b34b8ba895d1e99dd563acbde7
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2024-07-05 21:32:07 +0900

    Time: 86 ms (77.99%), Space: 72.1 MB (44.13%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+	const map = new Map();
+	
+	for(let i=0; i<nums.length; i++) {
+		const num = nums[i];
+		if(map.has(num) && i - map.get(num) <= k) {
+			return true;
+		}
+		map.set(num, i);
+	}
+	
+	return false;
+}
