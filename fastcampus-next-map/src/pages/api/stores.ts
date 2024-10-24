7c05bea0fdda8b90e6a3b169883e159f69f57fea
mustit-hepark <50111853+pullingoff@users.noreply.github.com>
2024-10-24 09:02:42 +0900

    chore: ch.18

--- fastcampus-next-map/src/pages/api/stores.ts
+++ fastcampus-next-map/src/pages/api/stores.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { StoreApiResponse, StoreType } from "@/types";
 import prisma from "@/db";
+import axios from "axios";
 
 interface ResponseType {
   page?: string;
@@ -15,35 +16,58 @@ export default async function handler(
 ) {
   const { page = "", limit = "", q, district }: ResponseType = req.query;
 
-  if (page) {
-    const count = await prisma.store.count();
-    const skipPage = parseInt(page) - 1; // 페이지 넘기는 역할
-    const stores = await prisma.store.findMany({
-      orderBy: { id: "asc" },
-      where: {
-        name: q ? { contains: q } : {},
-        address: district ? { contains: district } : {},
-      },
-      take: parseInt(limit),
-      skip: skipPage * 10,
+  if (req.method === "POST") {
+    // 데이터 생성
+    const formData = req.body;
+    const headers = {
+      Authorization: `KakaoAK ${process.env.KAKAO_CLIENT_ID}`,
+    };
+
+    const { data } = await axios.get(
+      `https://dapi.kakao.com/v2/local/search/address.json?query=${encodeURI(
+        formData.address,
+      )}`,
+      { headers },
+    );
+
+    const result = await prisma.store.create({
+      data: { ...formData, lat: data.documents[0].y, lng: data.documents[0].x },
     });
 
-    res.status(200).json({
-      page: parseInt(page),
-      data: stores,
-      totalCount: count,
-      totalPage: Math.ceil(count / 10),
-    });
+    return res.status(200).json(result);
   } else {
-    const { id }: { id?: string } = req.query;
+    // GET 요청 처리
 
-    const stores = await prisma.store.findMany({
-      orderBy: { id: "asc" },
-      where: {
-        id: id ? parseInt(id) : {},
-      },
-    });
+    if (page) {
+      const count = await prisma.store.count();
+      const skipPage = parseInt(page) - 1; // 페이지 넘기는 역할
+      const stores = await prisma.store.findMany({
+        orderBy: { id: "asc" },
+        where: {
+          name: q ? { contains: q } : {},
+          address: district ? { contains: district } : {},
+        },
+        take: parseInt(limit),
+        skip: skipPage * 10,
+      });
 
-    return res.status(200).json(id ? stores[0] : stores);
+      res.status(200).json({
+        page: parseInt(page),
+        data: stores,
+        totalCount: count,
+        totalPage: Math.ceil(count / 10),
+      });
+    } else {
+      const { id }: { id?: string } = req.query;
+
+      const stores = await prisma.store.findMany({
+        orderBy: { id: "asc" },
+        where: {
+          id: id ? parseInt(id) : {},
+        },
+      });
+
+      return res.status(200).json(id ? stores[0] : stores);
+    }
   }
 }
--- fastcampus-next-map/src/pages/stores/new.tsx
+++ fastcampus-next-map/src/pages/stores/new.tsx
@@ -1,64 +1,65 @@
-/*
-  This example requires some changes to your config:
-
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
-
+import { CATEGORY_ARR, FOOD_CERTIFY_ARR, STORE_TYPE_ARR } from "@/data/store";
+import axios from "axios";
 import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import { useRouter } from "next/router";
+import AddressSearch from "@/components/AddressSearch";
+import { StoreType } from "@/types";
 
 export default function StoreNewPage() {
+  const router = useRouter();
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors },
-  } = useForm();
-
+  } = useForm<StoreType>();
   return (
     <form
-      className="px-4 mt-10 md:max-w-4xl mx-auto py-8"
+      className="px-4 md:max-w-4xl mx-auto py-8"
       onSubmit={handleSubmit(async (data) => {
-        console.log(data);
+        try {
+          const result = await axios.post("/api/stores", data);
+          if (result.status === 200) {
+            // 성공 케이스
+            toast.success("맛집을 등록했습니다.");
+            router.replace(`/stores/${result?.data?.id}`);
+          } else {
+            // 실패 케이스
+            toast.error("다시 시도해주세요");
+          }
+        } catch (e) {
+          console.log(e);
+          toast.error("데이터 생성중 문제가 생겼습니다. 다시 시도해주세요.");
+        }
       })}
     >
       <div className="space-y-12">
         <div className="border-b border-gray-900/10 pb-12">
-          <div className="border-b border-gray-900/10 pb-12">
-            <h2 className="text-base font-semibold leading-7 text-gray-900">
-              맛집 등록
-            </h2>
-            <p className="mt-1 text-sm leading-6 text-gray-600">
-              맛집을 등록해주세요!
-            </p>
-          </div>
+          <h2 className="text-base font-semibold leading-7 text-gray-900">
+            맛집 등록
+          </h2>
+          <p className="mt-1 text-sm leading-6 text-gray-600">
+            아래 내용을 입력해서 맛집을 등록해주세요
+          </p>
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-3">
               <label
-                htmlFor="first-name"
+                htmlFor="name"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
                 가게명
               </label>
               <div className="mt-2">
                 <input
-                  id="first-name"
-                  {...register("name", { required: true })}
                   type="text"
-                  autoComplete="given-name"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  {...register("name", { required: true })}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 outline-none px-2 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
                 {errors?.name?.type === "required" && (
-                  <div className="pt-2 text-xs text-red-300">
-                    필수 입력 사항입니다.
+                  <div className="pt-2 text-xs text-red-600">
+                    필수 입력사항입니다.
                   </div>
                 )}
               </div>
@@ -66,91 +67,106 @@ export default function StoreNewPage() {
 
             <div className="sm:col-span-3">
               <label
-                htmlFor="last-name"
+                htmlFor="category"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
                 카테고리
               </label>
               <div className="mt-2">
-                <input
-                  id="last-name"
-                  name="last-name"
-                  type="text"
-                  autoComplete="family-name"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
+                <select
+                  {...register("category", { required: true })}
+                  className="block w-full rounded-md border-0 px-2 outline-none py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                >
+                  <option value="">카테고리 선택</option>
+                  {CATEGORY_ARR?.map((category) => (
+                    <option key={category} value={category}>
+                      {category}
+                    </option>
+                  ))}
+                </select>
+                {errors?.category?.type === "required" && (
+                  <div className="pt-2 text-xs text-red-600">
+                    필수 입력사항입니다.
+                  </div>
+                )}
               </div>
             </div>
 
             <div className="sm:col-span-4">
               <label
-                htmlFor="email"
+                htmlFor="phone"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
                 연락처
               </label>
               <div className="mt-2">
                 <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  {...register("phone", { required: true })}
+                  className="block w-full rounded-md border-0 outline-none px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
+                {errors?.phone?.type === "required" && (
+                  <div className="pt-2 text-xs text-red-600">
+                    필수 입력사항입니다.
+                  </div>
+                )}
               </div>
             </div>
-
-            <div className="col-span-full">
-              <label
-                htmlFor="street-address"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                주소 (다음 주소 검색)
-              </label>
-              <div className="mt-2">
-                <input
-                  id="street-address"
-                  name="street-address"
-                  type="text"
-                  autoComplete="street-address"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
+            <AddressSearch
+              setValue={setValue}
+              register={register}
+              errors={errors}
+            />
             <div className="sm:col-span-2 sm:col-start-1">
               <label
                 htmlFor="city"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
-                식품 인증구분
+                식품인증구분
               </label>
               <div className="mt-2">
-                <input
-                  id="city"
-                  name="city"
-                  type="text"
-                  autoComplete="address-level2"
-                  className="block w-full ro소unded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
+                <select
+                  {...register("foodCertifyName", { required: true })}
+                  className="block w-full rounded-md border-0 py-2 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                >
+                  <option value="">식품인증구분 선택</option>
+                  {FOOD_CERTIFY_ARR?.map((data) => (
+                    <option key={data} value={data}>
+                      {data}
+                    </option>
+                  ))}
+                </select>
+                {errors?.foodCertifyName?.type === "required" && (
+                  <div className="pt-2 text-xs text-red-600">
+                    필수 입력사항입니다.
+                  </div>
+                )}
               </div>
             </div>
 
             <div className="sm:col-span-2">
               <label
-                htmlFor="region"
+                htmlFor="storeType"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
-                업종 구분
+                업종구분
               </label>
               <div className="mt-2">
-                <input
-                  id="region"
-                  name="region"
-                  type="text"
-                  autoComplete="address-level1"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
+                <select
+                  {...register("storeType", { required: true })}
+                  className="block w-full rounded-md border-0 py-2 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                >
+                  <option value="">업종구분 선택</option>
+                  {STORE_TYPE_ARR?.map((data) => (
+                    <option key={data} value={data}>
+                      {data}
+                    </option>
+                  ))}
+                </select>
+                {errors?.storeType?.type === "required" && (
+                  <div className="pt-2 text-xs text-red-600">
+                    필수 입력사항입니다.
+                  </div>
+                )}
               </div>
             </div>
           </div>
@@ -160,6 +176,7 @@ export default function StoreNewPage() {
       <div className="mt-6 flex items-center justify-end gap-x-6">
         <button
           type="button"
+          onClick={() => router.back()}
           className="text-sm font-semibold leading-6 text-gray-900"
         >
           뒤로가기
