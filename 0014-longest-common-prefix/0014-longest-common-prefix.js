e9d888cd926229060607105ec384d701e691237f
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2025-02-02 14:01:26 +0900

    Time: 6 ms (19.16%), Space: 50.7 MB (29.87%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string[]} strs
+ * @return {string} 배열 원소들의 prefix 
+ for 문 하나에서 문자열 전체를 순회하면서 확인하기
+ prefix 아니면 바로 멈추고 리턴
+ */
+var longestCommonPrefix = function(strs) {
+    if(new Set(strs).size === 1) return strs[0];
+
+    strs.sort((a, b) => a.length - b.length) // 짧은 순으로 정렬
+
+    const shortestLength = strs[0].length;
+    const target = strs[0];
+    let idx = 99999999; // 최소 prefix 길이
+
+    for(let i=0; i<strs.length; i++) {
+        const word = strs[i];
+        let count = 0;
+        for(let j=0; j<shortestLength; j++) { // f l o w
+            if(word[j] === target[j]) {
+                count++;
+            } else {
+                break;
+            }
+        }
+        idx = Math.min(idx, count)
+    }
+
+    return target.slice(0, idx)
+};
