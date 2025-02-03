67a599d5fd213b7765010f161276ddbe8c648ba7
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2025-02-03 22:47:32 +0900

    Time: 371 ms (5.05%), Space: 58.8 MB (5.08%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -7,22 +7,15 @@ var isIsomorphic = function(s, t) {
     let dict = {} // {e: a, g: d} 매칭되는 것들 사전 {s: t}
 
     for(let i=0; i < s.length; i++) {
-        
-        
         // dict에 등록 안 되어있으면 등록
         if(Object.values(dict).includes(t[i])) { 
-            // 이떄 이미 다른거로 value가 등록돼있고, s[i]가 그 키 다르면 탈락
-            // console.log(Object.entries(dict))
-            // console.log('t[i]: ', t[i])
             const key = Object.entries(dict).find(elem => elem[1] === t[i])
-            // console.log(key, s[i])
             if(key[0] !== s[i]) return false;
         }
         if(!dict[s[i]]) {
             dict = {...dict, [s[i]]: t[i]}
-            continue;
-        }   
-        console.log(t[i])
+            // continue;
+        }
         if(dict[s[i]] !== t[i]) return false;
     }
     return true;
